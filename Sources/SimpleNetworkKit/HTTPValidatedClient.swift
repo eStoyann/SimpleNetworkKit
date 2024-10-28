0d16ba6da2ed18d63964d24ab03fc9d726ecf878
estoyan <stoyanchik1001@gmail.com>
2024-10-28 17:49:21 +0200

    added HTTPValidatedClient

---
+++ Sources/SimpleNetworkKit/HTTPValidatedClient.swift
@@ -0,0 +1,37 @@
+//
+//  HTTPValidatedClient.swift
+//  Network
+//
+//  Created by Evgeniy Stoyan on 28.10.2024.
+//
+import Foundation
+import SimpleNetworkKit
+
+public final class HTTPValidatedClient: HTTPClient {
+    private let client: HTTPClient
+    private let validator: HTTPURLResponseValidatable
+    
+    public init(client: HTTPClient = URLSession.shared,
+                validator: HTTPURLResponseValidatable = HTTPURLResponseValidator()) {
+        self.client = client
+        self.validator = validator
+    }
+    
+    public func fetch(request: URLRequest,
+               _ finished: @escaping CompletionHandler) -> HTTPURLSessionTask {
+        client.fetch(request: request) {[weak self] result in
+            guard let self else {return}
+            switch result {
+            case let .success((data, response)):
+                do {
+                    try self.validator.validate(response)
+                    finished(.success((data, response)))
+                } catch {
+                    finished(.failure(error))
+                }
+            case let .failure(error):
+                finished(.failure(error))
+            }
+        }
+    }
+}
